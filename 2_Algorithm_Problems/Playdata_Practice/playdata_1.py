ff58de60ff96c58de12b6a65a70c1dbdf590c7ad
Jay <37529449+skyla15@users.noreply.github.com>
2020-08-29 04:22:25 +0900

    Update playdata_1.py

--- 2_Algorithm_Problems/Playdata_Practice/playdata_1.py
+++ 2_Algorithm_Problems/Playdata_Practice/playdata_1.py
@@ -216,16 +216,15 @@ def solution(a, b):
 
 
 # 소수찾기 
-# "에라토스테네스의 체"
-
-import math
-
-def is_prime(x):
-    for i in range(1, int(math.sqrt(x))):
-        if x % i == 0 or x == 1:
-            return False
-        
-    return True
-
+# 에라토스테네스의 체!
+# n의 최대 약수 : sqrt(n) 이하 
 def solution(n):
-    return len([x for x in range(1,n+1) if is_prime(x)])
+    a = [False] * (n + 1)
+    m = int(n**0.5)
+    # sqrt(n)까지만 외부 반복
+    for i in range(2, m+1):
+        if a[i]: continue 
+        # 내부 반복 : n 까지 모든 수에 대해 True 값 -> 소수가 아님 
+        for j in range(i + i, n + 1, i):
+            a[j] = True        
+    return len([x for x in range(2, n + 1) if a[x] == False])
