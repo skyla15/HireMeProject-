2a0fee57207d90d9867b773271e97e7c050c3c18
Jay <37529449+skyla15@users.noreply.github.com>
2020-08-29 15:56:26 +0900

    Update playdata_1.py

--- 2_Algorithm_Problems/Playdata_Practice/playdata_1.py
+++ 2_Algorithm_Problems/Playdata_Practice/playdata_1.py
@@ -1,28 +1,28 @@
 # 두 정수 사이의 합
-def solution1(a, b):
+def solution(a, b):
     return sum(range(min(a,b),max(a,b)+1))
 
 # 수박수박수박수박수?
-def solution2(n):
+def solution(n):
     subak = '수박'
     return ''.join([subak[k%2] for k in range(n)])
 
 # 서울에서 김서방 찾기
-def solution3(seoul):
+def solution(seoul):
     return ''.join(['김서방은 ', str(seoul.index('Kim')), '에 있다'])
 
 # 약수의 합
-def solution4(n):
+def solution(n):
     return sum([k for k in range(1, n+1) if n % k == 0])
 
 # 문자열 내 p와 y의 개수
-def solution5(s):
+def solution(s):
     p = len([x for x in s.lower() if x == 'p'])
     y = len([x for x in s.lower() if x == 'y'])
     return p == y
 
 # 같은 숫자는 싫어
-def solution6(arr):
+def solution(arr):
     answer = []
     m, n = 0, 1
 
@@ -39,7 +39,7 @@ def solution6(arr):
     return answer
 
 # 가운데 글자 가져오기
-def solution7(s):
+def solution(s):
     answer = ''
     if len(s) == 1:
         return s
@@ -49,7 +49,7 @@ def solution7(s):
         return s[len(s)//2]
 
 # x만큼 간격이 있는 n개의 숫자
-def solution8(x, n):
+def solution(x, n):
     answer = []
     for i in range(n):
         answer.append(x + i * x)
@@ -57,7 +57,7 @@ def solution8(x, n):
     return answer
 
 # 직사각형 별찍기
-def solution9(a, b)
+def solution(a, b)
 # a, b = map(int, input().strip().split(' '))
     for i in range(b):
         for j in range(a):
@@ -65,12 +65,12 @@ def solution9(a, b)
         print()
 
 # 평균 구하기
-def solution10(arr):
+def solution(arr):
     return sum(arr)/len(arr)
 
 # 행렬의 덧셈
 import numpy as np
-def solution11(arr1, arr2):
+def solution(arr1, arr2):
     answer1 = np.array(arr1) + np.array(arr2)
     # return answer1.tolist()
     answer2 = [[None for col in range(len(arr1[0]))] for row in range(len(arr1))]
@@ -80,12 +80,12 @@ def solution11(arr1, arr2):
     return answer2
 
 # 짝수와 홀수
-def solution12(num):
+def solution(num):
     a = ['Even', 'Odd']
     return a[num%2]
 
 # 자릿수 더하기
-def solution13(n):
+def solution(n):
     answer = 0
     l = len(str(n))
     for i in range(l):
@@ -93,7 +93,7 @@ def solution13(n):
     return answer
 
 # 최대공약수와 최소공배수
-def solution14(n, m):
+def solution(n, m):
     # 1
     a = [i for i in range(1, n + 1) if n % i == 0]
     b = [i for i in range(1, m + 1) if m % i == 0]
@@ -110,46 +110,46 @@ def solution14(n, m):
 
 # 정수 제곱근 판별
 import math
-def solution15(n):
+def solution(n):
     sq = int(math.sqrt(n))
     if sq**2 == n:
         return (sq+1) ** 2
     return -1
 
 # 문자열 다루기 기본
-def solution16(s):
+def solution(s):
     if len(s) == 4 or len(s) == 6:
         if s.isdigit():
             return True
     return False
 
 # 정수 내림차순으로 배치하기
-def solution17(n):
+def solution(n):
     return int(''.join(sorted(str(n), reverse = True)))
 
 # 자연수 뒤집어 배열로 만들기
-def solution18(n):
+def solution(n):
     return [int(x) for x in str(n)[::-1]]
 
 # 핸드폰 번호 가리기
-def solution19(phone_number):
+def solution(phone_number):
     l = len(list(phone_number))
     return '*' * (l-4) + str(phone_number)[l-4:]
 
 # K번째 수
-def solution20(array, commands):
+def solution(array, commands):
     for a, b, c in commands:
         answer.append(sorted(array[a - 1:b])[c - 1])
     return answer
 
 
 # 하샤드 수
-def solution21(x):
+def solution(x):
     return x % sum([int(x) for x in str(x)]) == 0
 
 
 # 나누어 떨어지는 숫자배열
-def solution22(arr, divisor):
+def solution(arr, divisor):
     answer = [x for x in arr if x % divisor == 0]
     if len(answer) == 0:
         return [-1]
@@ -157,7 +157,7 @@ def solution22(arr, divisor):
 
 
 # 모의고사
-def solution23(answers):
+def solution(answers):
     temp = list()
     answer = list()
     a1 = [1, 2, 3, 4, 5]
@@ -230,7 +230,6 @@ def solution(n):
 
 
 # 예산
-#1 그리디 
 def solution(d, budget):
     answer = 0
     d.sort()
@@ -241,4 +240,40 @@ def solution(d, budget):
         else:
             answer += 1
     return answer
+
+
+# 체육복 
+def solution(n, lost, reserve):
+    reserves = [x for x in reserve if x not in lost]
+    lost = [x for x in lost if x not in reserve]
+
+    for r in reserves:
+        if r - 1 in lost:
+            lost.remove(r - 1)
+        elif r + 1 in lost:
+            lost.remove(r + 1) 
+            
+    return n - len(lost)
+
+
+# 시저 암호 
+def solution(s, n):
+    print(ord('a'), ord('z'), ord('A'), ord('Z'))
+    # 97 122 65 90 
+    answer = ''
+    for c in s:
+        if ord(c) >= 97 and ord(c) <= 122:
+            if ord(c) + n > 122:
+                answer += chr(97 + (ord(c) + n) % 123)
+            else:
+                answer += chr(ord(c) + n) 
+        elif ord(c) >= 65 and ord(c) <= 90:
+            if ord(c) + n > 90:
+                answer += chr(65 + (ord(c) + n) % 91) 
+            else:
+                answer += chr(ord(c) + n) 
+        else:
+            answer += c
+    return answer
+            
 
