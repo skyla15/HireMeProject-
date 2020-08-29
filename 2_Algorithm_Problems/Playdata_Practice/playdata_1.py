077da3bd2f0c22bc9b765400943b24a8498aee99
Jay <37529449+skyla15@users.noreply.github.com>
2020-08-29 18:54:00 +0900

    Update playdata_1.py

--- 2_Algorithm_Problems/Playdata_Practice/playdata_1.py
+++ 2_Algorithm_Problems/Playdata_Practice/playdata_1.py
@@ -306,3 +306,57 @@ def solution(n, arr1, arr2):
 
 # 심심해서 풀어보는 문제 - [카카오 인턴] 키패드 누르기. 
 # https://programmers.co.kr/learn/courses/30/lessons/67256
+from collections import deque
+
+def solution(numbers, hand):
+    answer = ''
+    keyboard = { 
+        1 : (0,0),
+        2 : (0,1),
+        3 : (0,2),
+        4 : (1,0),
+        5 : (1,1),
+        6 : (1,2),
+        7 : (2,0),
+        8 : (2,1),
+        9 : (2,2),
+        '*' : (3,0),
+        0 : (3,1),
+        '#' : (3,2)}
+    
+    # distance = |low index1 - low index2| + |col index1 - col index2|
+    r_deque, l_deque = deque(), deque()
+    l_deque.append('*')
+    r_deque.append('#')
+    for n in numbers:
+        left_distance = 0
+        right_distance = 0
+        if n in [1,4,7]:
+            answer += 'L'
+            l_deque.append(n)
+        elif n in [3,6,9]:
+            answer += 'R'
+            r_deque.append(n)
+        elif n in [2,5,8,0]:
+            l = l_deque[-1]
+            r = r_deque[-1] 
+            print(l, r)
+            for i in range(2):
+                left_distance += abs(keyboard[l][i] - keyboard[n][i])
+                right_distance += abs(keyboard[r][i] - keyboard[n][i])
+            print(left_distance, right_distance)
+            if left_distance > right_distance:
+                answer += 'R'
+                r_deque.append(n)
+            elif left_distance < right_distance:
+                answer += 'L'
+                l_deque.append(n)
+            elif left_distance == right_distance:
+                if hand == 'right':
+                    answer += 'R'
+                    r_deque.append(n)
+                elif hand == 'left':
+                    answer += 'L'
+                    l_deque.append(n)                   
+        
+    return answer
