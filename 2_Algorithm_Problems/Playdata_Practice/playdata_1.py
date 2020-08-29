03defa9dcde49471dbbf20948e4d10eb9c171638
Jay <37529449+skyla15@users.noreply.github.com>
2020-08-29 14:07:46 +0900

    Update playdata_1.py

--- 2_Algorithm_Problems/Playdata_Practice/playdata_1.py
+++ 2_Algorithm_Problems/Playdata_Practice/playdata_1.py
@@ -229,19 +229,16 @@ def solution(n):
     return len([x for x in range(2, n + 1) if a[x] == False])
 
 
-# 예산 - 그리디 / DFS
+# 예산
 #1 그리디 
 def solution(d, budget):
     answer = 0
     d.sort()
     while len(d) > 0:
         budget -= d.pop(0)
-        print(budget)
         if budget < 0:
             break;
         else:
             answer += 1
     return answer
-
-#2 DFS .... 
 
