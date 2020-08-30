231857275617bae3bbeb36b968d6057b8bdfc50a
Jay <skyla15@nate.com>
2020-08-30 20:01:14 +0900

    전화번호 목록[해쉬, Trie 문제]

---
+++ 2_Algorithm_Problems/PROGRAMMERS_LV2/전화번호목록.py
@@ -0,0 +1,20 @@
+# 노가다 문자열 비교가 필요하다 -> 해시
+# - 문자열 해싱 : 라빈 카프 / KMP 알고리즘
+# - 접두사 문제 : Trie
+## 트라이 접목 문제 : https://www.crocus.co.kr/1053
+
+_end = '_end'
+def trie(words):
+    root = dict()
+    for word in words:
+        current_dict = root
+        for letter in word:
+            if _end in current_dict:
+                return False
+            current_dict = current_dict.setdefault(letter, {})
+        current_dict[_end] = _end
+    return True
+
+def solution(phone_book):
+    phone_book.sort()
+    return trie(phone_book)
