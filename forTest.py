88b78fc6e29302c3e9e421197d870f0ed288df82
Jay <skyla15@nate.com>
2020-08-30 15:09:06 +0900

    전화번호목록[trie, 해시, 접두사]

--- forTest.py
+++ forTest.py
@@ -1,29 +1,21 @@
-element = None
-s = list()
+# Shallow Copy 활용한 풀이
 
-def push(element):
-    s.append(element)
-
-def pop():
-    if empty():
-        return False
-    return s.pop()
-
-def size():
-    return len(s)
-
-def empty():
-    return size() == 0
-
-def top():
-    return s[-1]
+'''
+B
+1) Shallow Copy
+A = list(B)
+# B와 다른 객체를 가르키는 객체 A
+# 하지만 각각의 객체가 참조하는 주소는 동일
+# 객체 안의 객체에는 적용 안됨
+ex)
+a = [[1,2],[3,4],[5,6]]
+b = list(a)
+id(a) == id(b) # False
+id(a[0]) == id(b[0]) # True
+a[0].append(100)
+print(b) # [[1,2,100],[3,4],[5,6]]
+'''
 
 
-push(1)
-push(2)
-push(3)
-
-print(pop())
-print(top())
-
+print(solution(['12', '134']))
 
