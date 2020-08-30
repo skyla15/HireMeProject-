ef04695985789e35c213444e71f430c202f56725
Jay <skyla15@nate.com>
2020-08-30 19:47:22 +0900

    조이스틱문제_리팩토링 필요

--- 2_Algorithm_Problems/PROGRAMMERS_LV2/조이스틱.py
+++ 2_Algorithm_Problems/PROGRAMMERS_LV2/조이스틱.py
@@ -1,15 +1,12 @@
-# 테스트케이스 11번 안됨
-'''
-    이동하면서 좌쪽 이동, 오른쪽 이동 계속 연산 필요
-'''
-
 from collections import deque
+
 
 def updown_count(c):
     if ord(c) >= 79:
         return ord('Z') - ord(c) + 1
     else:
         return ord(c) - 65
+
 
 def solution(name):
     non_a_deque = deque()
@@ -29,36 +26,33 @@ def solution(name):
 
     current_index = 0
     visited = list()
-    right_flag = False
-    left_flag = False
+    right_flag = left_flage = False
+
     while non_a_deque:
         mostleft = non_a_deque[0]
         mostright = non_a_deque[-1]
 
-        # print(mostleft, mostright)
-        # print(mostleft - current_index  <= (len(name) - mostright) - current_index)
-
         # 오른쪽으로 이동
         print(non_a_deque)
-        if abs(mostleft - current_index)  <= abs(mostright - current_index):
+        if mostleft - current_index <= len(name) - mostright + current_index:
+            if left_flag == True or right_flag == True:
+                current_index += 1
             right_flag = True
-            if left_flag == True:
-                current_index += 2
             while current_index <= mostleft:
                 print('right')
                 if name[current_index] != 'A' and current_index not in visited:
                     answer += updown_count(name[current_index])
                 answer += 1
-                print(name[current_index], 'current index : ', current_index, ' answer : ', answer)
+                # print(name[current_index], 'current index : ', current_index, ' answer : ', answer)
                 visited.append(current_index)
                 current_index += 1
             current_index -= 1
             non_a_deque.popleft()
         # 왼쪽으로 이동
         else:
-            left_flag = True
-            if right_flag == True:
+            if left_flag == True or right_flag == True:
                 current_index -= 1
+            left_flag = True
             for _ in range(current_index, mostright - len(name) - 1, -1):
                 print('left')
                 if name[current_index] != 'A' and current_index not in visited:
@@ -74,19 +68,16 @@ def solution(name):
     return answer
 
 
-print('answer : ', solution('ABAAAABB'))
-# print(solution('JAN'))
-# print(solution('JEROEN'))
-
-
-
+# print('answer : ', solution('ABABA'))
+print(solution('JAN'))  # 23
+print(solution('JEROEN'))  # 56
 
 '''
 # 11번 케이스 안돌아감 -> 좌우 계속 비교하면서 이동해야됨. 단 방향 이동 안됨 
 from collections import deque
 
 def solution(name):
-    
+
     non_a_deque = deque()
     answer = 0
     # name = 'AABAAAAAAABBB'
@@ -103,10 +94,10 @@ def solution(name):
         else:
             answer += ord(name[0]) - 65
         return answer
-    
+
     elif len(non_a_deque) == 0:
         return answer
-    
+
     elif non_a_deque[0] <= len(name) - non_a_deque[-1]:
         # 오른쪽으로 이동, 마지막 non_a_deque 인덱쓰까지 이동 
         for i in range(non_a_deque[-1] + 1):
@@ -121,7 +112,7 @@ def solution(name):
             print('right  : ', answer, name[i], i)
         answer -= 1
         print('answer : ', answer)
-        
+
     else:
         # 왼쪽으로 이동, 처음 non_a_dque 인덱스까지 이동  
         for i in range(0, non_a_deque[0] - len(name) - 1 , -1):
